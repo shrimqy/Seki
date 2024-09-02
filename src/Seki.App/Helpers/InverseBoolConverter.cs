68054d1b04efe9628fdc05c8d1529767ec587b9d
shrimpyvongue <shrimpyvongue@gmail.com>
2024-09-02 11:19:41 +0530

    Inverse Boolean Converter Function To Conditionally Load Grouped Messages

---
+++ src/Seki.App/Helpers/InverseBoolConverter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seki.App.Helpers
+{
+    public class InverseBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is bool boolean)
+            {
+                return !boolean;
+            }
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
